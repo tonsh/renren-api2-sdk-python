7910d45ea5b89ad29f5c4dc49929c30ff4434722
tonsh <tonshlee@gmail.com>
2013-10-17 12:05:03 +0800

    Fixed: Complement RenrenAPIError & ErrorCode

--- renren_client/error.py
+++ renren_client/error.py
@@ -3,18 +3,21 @@
 class RenrenAPIError(Exception):
     """ Renren API exception class."""
     def __init__(self, code, msg):
-        self.code = code
+        self.type, self.code = code.split('.')
+        self.code = self.code.upper()
         self.msg = msg
 
     def __repr__(self):
         return '<RenrenAPIError Class>'
 
     def __unicode__(self):
-        return 'RenrenAPIError: %s - %s' % (self.code, self.msg)
+        return '%s.%s - %s' % (self.type, self.code, self.msg)
 
     def __str__(self):
         return unicode(self).encode('utf-8')
 
+
 class ErrorCode:
-    SERVICE_UNAVAILABLE = 1
-    EXPIRED_TOKEN = 7
+    SERVICE_UNAVAILABLE = 'SERVICE_UNAVAILABLE '
+    SERVICE_BUSY = 'SERVICE_BUSY'
+    EXPIRED_TOKEN = 'EXPIRED-TOKEN'
